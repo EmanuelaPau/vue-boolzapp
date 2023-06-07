994840d77ed8cfe8861ebc1ff423d256ff64ea43
EmanuelaPau <uapame@gmail.com>
2023-06-07 15:35:46 +0200

    Add contact index

--- script/my_script.js
+++ script/my_script.js
@@ -1,7 +1,15 @@
 console.log('hello JS');
 
-// M1
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v -for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+
+// Click sul contatto mostra la conversazione del contatto cliccato
+
+
+// Milestone 3
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
 
 const { createApp } = Vue;
 
@@ -9,6 +17,7 @@ createApp({
     data() {
         return {
             // Contacts array 
+            contactIndex: 0,
             contacts: [
                 {
                     name: 'Michele',
@@ -177,12 +186,14 @@ createApp({
                         }
                     ],
                 }
-            ]
-
+            ],
 
         }
     },
     methods: {
 
+        addMessages() {
+            console.log('prova');
+        }
     }
 }).mount('#app') 
